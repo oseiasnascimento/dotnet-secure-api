92b473ecd11d867abfe7a6e46a06dea9609e50a8
Oseias Nascimento <oseiassaboia2@gmail.com>
2025-08-28 15:21:25 -0300

    feature: Adicionado o contrato para salvar e remover arquivos via UploadFile

---
+++ src/Guardian.Application/Shared/Contracts/IUploadFileService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Guardian.Application.Shared.Dtos;
+using Guardian.Application.Wrappers;
+
+namespace Guardian.Application.Shared.Contracts
+{
+    public interface IUploadFileService
+    {
+        public Task<Response<string>> UploadFileAsync(UploadFileRequest request, IFormFile file, string type);
+        public Task<Response<string>> RemoveFileAsync(string filePath);
+    }
+}
