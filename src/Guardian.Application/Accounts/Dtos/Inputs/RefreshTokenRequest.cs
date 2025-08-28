03b462f8c36652b5037cbb332c1b0ece97d19cc0
Oseias Nascimento <oseiassaboia2@gmail.com>
2025-08-28 15:10:03 -0300

    feature: Adicionado o Dtos de requicisão RefreshToken

---
+++ src/Guardian.Application/Accounts/Dtos/Inputs/RefreshTokenRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace Guardian.Application.Accounts.Dtos.Inputs
+{
+    public class RefreshTokenRequest
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+
+        public RefreshTokenRequest(string accessToken, string refreshToken)
+        {
+            AccessToken = accessToken;
+            RefreshToken = refreshToken;
+        }
+    }
+}
