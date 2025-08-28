74f106ef600c9c318c140cae3969b99188360cff
Oseias Nascimento <oseiassaboia2@gmail.com>
2025-08-28 20:15:28 -0300

    feature: Criada a entidade abstrata que serve como base para criação de outras entidades.

---
+++ src/Guardian.Domain/Common/Entities/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Guardian.Domain.Common.Entities
+{
+    /// <summary>
+    /// Entidade abstrata que serve como base para criação de outras entidades.
+    /// </summary>
+    public abstract class Entity
+    {
+        [Column("id")]
+        public int Id { get; set; }
+    }
+}
