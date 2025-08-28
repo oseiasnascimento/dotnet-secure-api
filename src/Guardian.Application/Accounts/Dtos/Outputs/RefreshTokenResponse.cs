b405638737a3745876fb326244c299a95a8ecda8
Oseias Nascimento <oseiassaboia2@gmail.com>
2025-08-28 15:13:10 -0300

    feature: Adicionado o Dtos de resposta do RefreshToken

---
+++ src/Guardian.Application/Accounts/Dtos/Outputs/RefreshTokenResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Guardian.Application.Accounts.Dtos.Outputs
+{
+    public class RefreshTokenResponse
+    {
+        [JsonIgnore]
+        public string AccessToken { get; set; }
+
+        [JsonIgnore]
+        public string RefreshToken { get; set; }
+    }
+}
