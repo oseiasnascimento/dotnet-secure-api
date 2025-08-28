4959d9616daa2a8a924f5654573b906324940ffc
Oseias Nascimento <oseiassaboia2@gmail.com>
2025-08-28 15:12:09 -0300

    feature: Adicionado o Dtos de resposta de autenticação

---
+++ src/Guardian.Application/Accounts/Dtos/Outputs/AuthenticationResponse.cs
@@ -0,0 +1,32 @@
+﻿using Guardian.Domain.User.Entities;
+using System.Text.Json.Serialization;
+
+namespace Guardian.Application.Accounts.Dtos.Outputs
+{
+    public class AuthenticationResponse
+    {
+        public string Email { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsFirstLogin { get; set; }
+
+        [JsonIgnore]
+        public string AccessToken { get; set; }
+
+        [JsonIgnore]
+        public string RefreshToken { get; set; }
+
+        public static AuthenticationResponse Map(ApplicationUser user, IEnumerable<string> roles, string accessToken, string refreshToken)
+        {
+            return new()
+            {
+                Email = user.Email,
+                IsActive = user.IsActive,
+                Roles = roles,
+                AccessToken = accessToken,
+                RefreshToken = refreshToken,
+                IsFirstLogin = user.IsDefaultPassword || user.LastLogin is null
+            };
+        }
+    }
+}
