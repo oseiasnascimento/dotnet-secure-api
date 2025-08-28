1c9699dadfa5795099e7a7c9098ca80f6ca3ff21
Oseias Nascimento <oseiassaboia2@gmail.com>
2025-08-28 15:10:55 -0300

    feature: Adicionado o Dtos de requicisão de alterar senha

---
+++ src/Guardian.Application/Accounts/Dtos/Inputs/ResetPasswordRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Guardian.Application.Accounts.Dtos.Inputs
+{
+    public class ResetPasswordRequest
+    {
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
