0656cb4f9fb122368cf738fe9b6e9060152c0704
Oseias Nascimento <oseiassaboia2@gmail.com>
2025-08-28 20:18:52 -0300

    feature: Criada a entidade de usuarios

---
+++ src/Guardian.Domain/User/Entities/ApplicationRole.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Guardian.Domain.User.Entities
+{
+    /// <summary>
+    /// Cargos do sistema.
+    /// </summary>
+    public class ApplicationRole : IdentityRole<int>
+    {
+        public string Description { get; set; }
+        public List<UserRole> UserRoles { get; set; }
+
+        public static ApplicationRole Create(string name, string description)
+        {
+            return new()
+            {
+                Name = name,
+                NormalizedName = name.ToUpper().Trim(),
+                Description = description,
+                ConcurrencyStamp = Guid.NewGuid().ToString()
+            };
+        }
+    }
+}
