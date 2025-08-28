d78589a2ea9f322ddf539335f0dddddbcb2f321e
Oseias Nascimento <oseiassaboia2@gmail.com>
2025-08-28 15:20:19 -0300

    feature: Adicionado o contrato para gerar e salvar um novo log do tipo Information ou Warning no Banco de Dados

---
+++ src/Guardian.Application/Shared/Contracts/ILoggerService.cs
@@ -0,0 +1,19 @@
+﻿using Guardian.Application.Shared.Dtos;
+
+namespace Guardian.Application.Shared.Contracts
+{
+    public interface ILoggerService
+    {
+        /// <summary>
+        /// Gera e salva um novo log do tipo Information no Banco de Dados.
+        /// </summary>
+        /// <param name="request">Request necessária para salvar um novo log.</param>
+        public void LogInfo(CreateLogRequest request);
+
+        /// <summary>
+        /// Gera e salva um novo log do tipo Warning no Banco de Dados.
+        /// </summary>
+        /// <param name="request">Request necessária para salvar um novo log.</param>
+        public void LogWarning(CreateLogRequest request);
+    }
+}
