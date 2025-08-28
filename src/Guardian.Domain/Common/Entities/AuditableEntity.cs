f044dfa48ce173337f2cb0e1b01de31f032ea504
Oseias Nascimento <oseiassaboia2@gmail.com>
2025-08-28 20:15:51 -0300

    feature: Criada a entidade auditável abstrata que serve como base para criação de outras entidades.

---
+++ src/Guardian.Domain/Common/Entities/AuditableEntity.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Guardian.Domain.Common.Entities
+{
+    /// <summary>
+    /// Entidade auditável abstrata que serve como base para criação de outras entidades.
+    /// Herda de Entity.
+    /// </summary>
+    public class AuditableEntity : Entity
+    {
+        [Column("criado_por")]
+        public int CreatedBy { get; set; }
+        [Column("criado_em")]
+        public DateTime CreatedAt { get; set; }
+
+        [Column("atualizado_por")]
+        public int LastUpdatedBy { get; set; }
+        [Column("atualizado_em")]
+        public DateTime LastUpdatedAt { get; set; }
+
+        public AuditableEntity()
+        {
+            CreatedAt = DateTime.Now;
+            LastUpdatedAt = DateTime.Now;
+        }
+    }
+}
