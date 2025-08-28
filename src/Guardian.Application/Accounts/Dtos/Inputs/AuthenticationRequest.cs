9af4c648c976627d39cc6d0f5a699ada1bcf93cb
Oseias Nascimento <oseiassaboia2@gmail.com>
2025-08-28 15:08:39 -0300

    feature: Adicionado o Dtos de Authentication

---
+++ src/Guardian.Application/Accounts/Dtos/Inputs/AuthenticationRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Guardian.Application.Accounts.Dtos.Inputs
+{
+    public class AuthenticationRequest
+    {
+        public string CPF { get; set; }
+        public string Password { get; set; }
+    }
+}
