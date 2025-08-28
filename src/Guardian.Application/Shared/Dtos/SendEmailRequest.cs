6c173edd675f0209f6f4ecee52a273994fe660ea
Oseias Nascimento <oseiassaboia2@gmail.com>
2025-08-28 15:26:13 -0300

    feature: Adicionado o Dtos de requicisão para padronização de envia de email

---
+++ src/Guardian.Application/Shared/Dtos/SendEmailRequest.cs
@@ -0,0 +1,11 @@
+namespace Guardian.Application.Shared.Dtos
+{
+    public class SendMailRequest
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string TemplatePath { get; set; }
+        public Dictionary<string, string> Parameters { get; set; } = [];
+    }
+}
