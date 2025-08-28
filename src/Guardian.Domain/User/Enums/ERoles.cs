cd6f9ca0ee1db3fd943fc10510ed33c7192d743f
Oseias Nascimento <oseiassaboia2@gmail.com>
2025-08-28 20:20:45 -0300

    feature: Enum para cargos existentes no sistema

---
+++ src/Guardian.Domain/User/Enums/ERoles.cs
@@ -0,0 +1,28 @@
+﻿namespace Guardian.Domain.User.Enums
+{
+    /// <summary>
+    /// Cargos existentes no sistema
+    /// </summary>
+    public enum ERoles
+    {
+        /// <summary>
+        /// Super Administrador.
+        /// </summary>
+        SuperAdmin,
+
+        /// <summary>
+        /// Administrador.
+        /// </summary>
+        Admin,
+
+        /// <summary>
+        /// Usuário.
+        /// </summary>
+        User,
+
+        /// <summary>
+        /// Apenas leitura e visualização.
+        /// </summary>
+        ReadOnly
+    }
+}
