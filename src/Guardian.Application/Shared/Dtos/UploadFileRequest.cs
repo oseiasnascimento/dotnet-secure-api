3b6ce296517bef7bfd292b881f59148c1b50f6ee
Oseias Nascimento <oseiassaboia2@gmail.com>
2025-08-28 15:27:11 -0300

    feature: Adicionado o Dtos de requicisão para envia de arquivos

---
+++ src/Guardian.Application/Shared/Dtos/UploadFileRequest.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Guardian.Application.Shared.Dtos
+{
+    public class UploadFileRequest
+    {
+        public string Name { get; set; }
+        public string Extension { get; set; }
+        public string Path { get; set; }
+    }
+}
