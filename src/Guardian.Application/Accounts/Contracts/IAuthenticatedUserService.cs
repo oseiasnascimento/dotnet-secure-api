e3a9688cc0e2ec85a2e86e14a78cb6552a9eca31
Oseias Nascimento <oseiassaboia2@gmail.com>
2025-08-28 15:06:19 -0300

    feature: Adicionado o contrato dos serviços de AuthenticateUser

---
+++ src/Guardian.Application/Accounts/Contracts/IAuthenticatedUserService.cs
@@ -0,0 +1,13 @@
+﻿namespace Guardian.Application.Accounts.Contracts
+{
+    public interface IAuthenticatedUserService
+    {
+        /// <summary>
+        /// Id do usuário autenticado.
+        /// </summary>
+        public int Id { get; }
+
+        public int GetId();
+
+    }
+}
