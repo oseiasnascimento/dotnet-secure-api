1b6c9209599e905ffa9a98b5da665fd7c904d62f
Oseias Nascimento <oseiassaboia2@gmail.com>
2025-08-28 15:25:40 -0300

    feature: Adicionado o Dtos de requicisão para a criação de Logs

---
+++ src/Guardian.Application/Shared/Dtos/CreateLogRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace Guardian.Application.Shared.Dtos
+{
+    public class CreateLogRequest
+    {
+        public string Action { get; set; }
+        public string Message { get; set; }
+        public string Request { get; set; }
+        public string Response { get; set; }
+        public string UserId { get; set; }
+
+        public CreateLogRequest()
+        {
+
+        }
+
+        public CreateLogRequest(string request, string response)
+        {
+            Request = request;
+            Response = response;
+        }
+    }
+}
