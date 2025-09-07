57ba301f6feaec78acd3105873e03959bfe97156
Oseias Nascimento <oseiassaboia2@gmail.com>
2025-09-07 10:35:04 -0300

    feature: Adicionado as configurações para loger do sistema

---
+++ src/Guardian.Infrastructure.Logging/Services/LoggingService.cs
@@ -0,0 +1,27 @@
+﻿using Seletivo.Application.Shared.Contracts;
+using Seletivo.Application.Shared.Dtos;
+using Serilog;
+
+namespace Infrastructure.Shared.Services
+{
+    public class LoggerService : ILoggerService
+    {
+        public void LogInfo(CreateLogRequest request)
+        {
+            Log.ForContext("Action", request.Action)
+                        .ForContext("Request", request.Request)
+                        .ForContext("Response", request.Response)
+                        .ForContext("UserId", request.UserId)
+                        .Information(request.Message);
+        }
+
+        public void LogWarning(CreateLogRequest request)
+        {
+            Log.ForContext("Action", request.Action)
+                        .ForContext("Request", request.Request)
+                        .ForContext("Response", request.Response)
+                        .ForContext("UserId", null)
+                        .Warning(request.Message);
+        }
+    }
+}
