b02ea4bf823588d3921ad5fb5a5e3f0183149dbd
Oseias Nascimento <oseiassaboia2@gmail.com>
2025-09-09 19:26:00 -0300

    feature: desenvolvido o BaseController genérico que serve como base para criação de outras rotas.

---
+++ src/Guardian.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+using Guardian.Application.Accounts.Contracts;
+using Guardian.Application.Shared.Contracts;
+
+namespace Guardian.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1")]
+    [EnableRateLimiting("fixed")]
+    public class BaseController : Controller
+    {
+        private IAuthenticatedUserService _authenticatedUser;
+        protected IAuthenticatedUserService AuthenticatedUser => _authenticatedUser ??= HttpContext.RequestServices.GetService<IAuthenticatedUserService>();
+
+        private ILoggerService _logger;
+        protected ILoggerService Logger => _logger ??= HttpContext.RequestServices.GetService<ILoggerService>();
+    }
+}
